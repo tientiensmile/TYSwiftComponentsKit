c400821f6a7d54d99194b7d25d18b45125e5992d
audrey_wang@cmoney.com.tw <audrey_wang@cmoney.com.tw>
2022-02-28 10:01:13 +0800

    feat: add demo target

---
+++ TYSwiftComponentsKit/DemoTYSwiftComponents/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DemoTYSwiftComponents
+//
+//  Created by Tienyun Wang on 2022/2/28.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ TYSwiftComponentsKit/DemoTYSwiftComponents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoTYSwiftComponents
+//
+//  Created by Tienyun Wang on 2022/2/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
